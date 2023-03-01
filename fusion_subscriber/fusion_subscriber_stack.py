693ad65a9c6ba20c02a8e27b84ad9a1f8629f032
dev1 <jwowshubham@gmail.com>
2023-03-01 11:08:19 +0530

    added l2 costruct for dynamodb and s3

--- fusion_subscriber/fusion_subscriber_stack.py
+++ fusion_subscriber/fusion_subscriber_stack.py
@@ -35,7 +35,7 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_s3 as s3,
     aws_dynamodb as dynamodb,
-    # core
+    # core as cdk
 )
 from constructs import Construct
 
@@ -45,43 +45,66 @@ class FusionSubscriberStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         
         
-        s3.CfnBucket( self, "fusion-bucket-1",bucket_name="fusion-bucket-2",
-                        access_control="Private", 
-                        bucket_encryption=s3.CfnBucket.BucketEncryptionProperty( 
-                            server_side_encryption_configuration=[ 
-                                s3.CfnBucket.ServerSideEncryptionRuleProperty( 
-                                    server_side_encryption_by_default=s3.CfnBucket.ServerSideEncryptionByDefaultProperty( 
-                                        sse_algorithm="AES256" ) ) ] ), 
-                        public_access_block_configuration=s3.BlockPublicAccess.BLOCK_ALL,
-                        tags= [{'key': 'environment', 'value': 'development'}],
-                        versioning_configuration=s3.CfnBucket.VersioningConfigurationProperty(status="Enabled"))
+        # s3.CfnBucket( self, "fusion-bucket-1",bucket_name="fusion-bucket-2",
+        #                 access_control="Private", 
+        #                 bucket_encryption=s3.CfnBucket.BucketEncryptionProperty( 
+        #                     server_side_encryption_configuration=[ 
+        #                         s3.CfnBucket.ServerSideEncryptionRuleProperty( 
+        #                             server_side_encryption_by_default=s3.CfnBucket.ServerSideEncryptionByDefaultProperty( 
+        #                                 sse_algorithm="AES256" ) ) ] ), 
+        #                 public_access_block_configuration=s3.BlockPublicAccess.BLOCK_ALL,
+        #                 tags= [{'key': 'environment', 'value': 'development'}],
+        #                 versioning_configuration=s3.CfnBucket.VersioningConfigurationProperty(status="Enabled"))
 
 
-        dynamodb.CfnTable( self, "MyCfnTable", 
-            key_schema=[dynamodb.CfnTable.KeySchemaProperty( 
-                      attribute_name="ref_event_uid", 
-                      #hash -- partition key - RANGE - sort key
-                      key_type="HASH"
-            )],
+        alertbucket= s3.Bucket(self, "fusion-bucket-1",
+                          bucket_name="fusion-bucket-2",
+                          block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+                          bucket_key_enabled= False , encryption= s3.BucketEncryption.S3_MANAGED,
+                          versioned= True,
+                        #   RemovalPolicy=s3.RemovalPolicy.DESTROY                                     
+                          
+                          )
+        
+        alertdynaomodb = dynamodb.Table(self, "MyCfnTable",
+                        table_name= "fusiondynamodbtable",
+                        encryption= dynamodb.TableEncryption.AWS_MANAGED,
+                        partition_key=dynamodb.Attribute(name="ref_event_uid", type=dynamodb.AttributeType.STRING),
+                #     replication_regions=["us-east-1", "us-east-2", "us-west-2"],
+                        billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+                        table_class=dynamodb.TableClass.STANDARD
 
-                # the properties below are optional
-            attribute_definitions=[dynamodb.CfnTable.AttributeDefinitionProperty(
-            # The data type for the attribute, where:. - S - the attribute is of type String - N - the attribute is of type Number - B - the attribute is of type Binary
-                attribute_name="ref_event_uid",
-                attribute_type="N"
-            )],
-            billing_mode="PAY_PER_REQUEST",
-            # Valid values include: - PROVISIONED - We recommend using PROVISIONED for predictable workloads. PROVISIONED sets the billing mode to Provisioned Mode . - PAY_PER_REQUEST - We recommend using PAY_PER_REQUEST for unpredictable workloads. PAY_PER_REQUEST sets the billing mode to On-Demand Mode . If not specified, the default is PROVISIONED 
+                 )
+
+
+
+
+
+        # dynamodb.CfnTable( self, "MyCfnTable", 
+        #     key_schema=[dynamodb.CfnTable.KeySchemaProperty( 
+        #               attribute_name="ref_event_uid", 
+        #               #hash -- partition key - RANGE - sort key
+        #               key_type="HASH"
+        #     )],
+
+        #         # the properties below are optional
+        #     attribute_definitions=[dynamodb.CfnTable.AttributeDefinitionProperty(
+        #     # The data type for the attribute, where:. - S - the attribute is of type String - N - the attribute is of type Number - B - the attribute is of type Binary
+        #         attribute_name="ref_event_uid",
+        #         attribute_type="N"
+        #     )],
+        #     billing_mode="PAY_PER_REQUEST",
+        #     # Valid values include: - PROVISIONED - We recommend using PROVISIONED for predictable workloads. PROVISIONED sets the billing mode to Provisioned Mode . - PAY_PER_REQUEST - We recommend using PAY_PER_REQUEST for unpredictable workloads. PAY_PER_REQUEST sets the billing mode to On-Demand Mode . If not specified, the default is PROVISIONED 
          
-            sse_specification=dynamodb.CfnTable.SSESpecificationProperty(
-                sse_enabled=True,
+        #     sse_specification=dynamodb.CfnTable.SSESpecificationProperty(
+        #         sse_enabled=True,
 
-            #     # the properties below are optional
-            #     kms_master_key_id="kmsMasterKeyId",
-            #     sse_type="sseType"
-             ),
-            table_class="STANDARD",
-            table_name="fusiondynamodbtable",
-            tags= [{'key': 'environment', 'value': 'development'}],              
+        #     #     # the properties below are optional
+        #     #     kms_master_key_id="kmsMasterKeyId",
+        #     #     sse_type="sseType"
+        #      ),
+        #     table_class="STANDARD",
+        #     table_name="fusiondynamodbtable",
+        #     tags= [{'key': 'environment', 'value': 'development'}],              
             
-            )
+        #     )
