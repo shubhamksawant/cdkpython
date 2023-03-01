eda29d50eedf5be68ddbad3a535338e63094a989
dev1 <jwowshubham@gmail.com>
2023-03-01 13:27:11 +0530

    added l2 construct

--- fusion_subscriber/fusion_subscriber_stack.py
+++ fusion_subscriber/fusion_subscriber_stack.py
@@ -36,6 +36,7 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_dynamodb as dynamodb,
     aws_opensearchservice as opensearchservice,
+    aws_ec2 as ec2
     # core
 )
 from constructs import Construct
@@ -46,75 +47,107 @@ class FusionSubscriberStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         
         
-        s3.CfnBucket( self, "fusion-bucket-1",bucket_name="fusion-bucket-2",
-                        access_control="Private", 
-                        bucket_encryption=s3.CfnBucket.BucketEncryptionProperty( 
-                            server_side_encryption_configuration=[ 
-                                s3.CfnBucket.ServerSideEncryptionRuleProperty( 
-                                    server_side_encryption_by_default=s3.CfnBucket.ServerSideEncryptionByDefaultProperty( 
-                                        sse_algorithm="AES256" ) ) ] ), 
-                        public_access_block_configuration=s3.BlockPublicAccess.BLOCK_ALL,
-                        tags= [{'key': 'environment', 'value': 'development'}],
-                        versioning_configuration=s3.CfnBucket.VersioningConfigurationProperty(status="Enabled"))
+        # s3.CfnBucket( self, "fusion-bucket-1",bucket_name="fusion-bucket-2",
+        #                 access_control="Private", 
+        #                 bucket_encryption=s3.CfnBucket.BucketEncryptionProperty( 
+        #                     server_side_encryption_configuration=[ 
+        #                         s3.CfnBucket.ServerSideEncryptionRuleProperty( 
+        #                             server_side_encryption_by_default=s3.CfnBucket.ServerSideEncryptionByDefaultProperty( 
+        #                                 sse_algorithm="AES256" ) ) ] ), 
+        #                 public_access_block_configuration=s3.BlockPublicAccess.BLOCK_ALL,
+        #                 tags= [{'key': 'environment', 'value': 'development'}],
+        #                 versioning_configuration=s3.CfnBucket.VersioningConfigurationProperty(status="Enabled"))
 
 
-        dynamodb.CfnTable( self, "MyCfnTable", 
-            key_schema=[dynamodb.CfnTable.KeySchemaProperty( 
-                      attribute_name="ref_event_uid", 
-                      #hash -- partition key - RANGE - sort key
-                      key_type="HASH"
-            )],
+        # dynamodb.CfnTable( self, "MyCfnTable", 
+        #     key_schema=[dynamodb.CfnTable.KeySchemaProperty( 
+        #               attribute_name="ref_event_uid", 
+        #               #hash -- partition key - RANGE - sort key
+        #               key_type="HASH"
+        #     )],
 
-                # the properties below are optional
-            attribute_definitions=[dynamodb.CfnTable.AttributeDefinitionProperty(
-            # The data type for the attribute, where:. - S - the attribute is of type String - N - the attribute is of type Number - B - the attribute is of type Binary
-                attribute_name="ref_event_uid",
-                attribute_type="N"
-            )],
-            billing_mode="PAY_PER_REQUEST",
-            # Valid values include: - PROVISIONED - We recommend using PROVISIONED for predictable workloads. PROVISIONED sets the billing mode to Provisioned Mode . - PAY_PER_REQUEST - We recommend using PAY_PER_REQUEST for unpredictable workloads. PAY_PER_REQUEST sets the billing mode to On-Demand Mode . If not specified, the default is PROVISIONED 
+        #         # the properties below are optional
+        #     attribute_definitions=[dynamodb.CfnTable.AttributeDefinitionProperty(
+        #     # The data type for the attribute, where:. - S - the attribute is of type String - N - the attribute is of type Number - B - the attribute is of type Binary
+        #         attribute_name="ref_event_uid",
+        #         attribute_type="N"
+        #     )],
+        #     billing_mode="PAY_PER_REQUEST",
+        #     # Valid values include: - PROVISIONED - We recommend using PROVISIONED for predictable workloads. PROVISIONED sets the billing mode to Provisioned Mode . - PAY_PER_REQUEST - We recommend using PAY_PER_REQUEST for unpredictable workloads. PAY_PER_REQUEST sets the billing mode to On-Demand Mode . If not specified, the default is PROVISIONED 
          
-            sse_specification=dynamodb.CfnTable.SSESpecificationProperty(
-                sse_enabled=True,
+        #     sse_specification=dynamodb.CfnTable.SSESpecificationProperty(
+        #         sse_enabled=True,
 
-            #     # the properties below are optional
-            #     kms_master_key_id="kmsMasterKeyId",
-            #     sse_type="sseType"
-             ),
-            table_class="STANDARD",
-            table_name="fusiondynamodbtable",
-            tags= [{'key': 'environment', 'value': 'development'}],              
+        #     #     # the properties below are optional
+        #     #     kms_master_key_id="kmsMasterKeyId",
+        #     #     sse_type="sseType"
+        #      ),
+        #     table_class="STANDARD",
+        #     table_name="fusiondynamodbtable",
+        #     tags= [{'key': 'environment', 'value': 'development'}],              
             
-            )
+        #     )
         
 
-        opensearchservice.CfnDomain(self, "MyCfnDomain",
-                 domain_name="fusion1",
-                 engine_version="OpenSearch_1.1",
-                 cluster_config=opensearchservice.CfnDomain.ClusterConfigProperty(
-                     dedicated_master_count=3,
-                     dedicated_master_enabled=True,
-                     dedicated_master_type="r6g.large.search",
-                     instance_count=3,
-                     instance_type="r6g.large.search",
-                    #  warm_count=3,
-                    #  warm_enabled=False,
-                    #  warm_type="r6g.large.search",
-                    #  zone_awareness_config=opensearchservice.CfnDomain.ZoneAwarenessConfigProperty(
-                    #     availability_zone_count=3
-                    #  ),
-                     zone_awareness_enabled=False
-                ),
-                ebs_options=opensearchservice.CfnDomain.EBSOptionsProperty(
-                     ebs_enabled=True,
-                     iops=3000,
-                     throughput=125,
-                     volume_size=50,
-                     volume_type="gp3"
-                ),
-                tags= [{'key': 'environment', 'value': 'development'}],
+        # opensearchservice.CfnDomain(self, "MyCfnDomain",
+        #          domain_name="fusion1",
+        #          engine_version="OpenSearch_1.1",
+        #          cluster_config=opensearchservice.CfnDomain.ClusterConfigProperty(
+        #              dedicated_master_count=3,
+        #              dedicated_master_enabled=True,
+        #              dedicated_master_type="r6g.large.search",
+        #              instance_count=3,
+        #              instance_type="r6g.large.search",
+        #             #  warm_count=3,
+        #             #  warm_enabled=False,
+        #             #  warm_type="r6g.large.search",
+        #             #  zone_awareness_config=opensearchservice.CfnDomain.ZoneAwarenessConfigProperty(
+        #             #     availability_zone_count=3
+        #             #  ),
+        #              zone_awareness_enabled=False
+        #         ),
+        #         ebs_options=opensearchservice.CfnDomain.EBSOptionsProperty(
+        #              ebs_enabled=True,
+        #              iops=3000,
+        #              throughput=125,
+        #              volume_size=50,
+        #              volume_type="gp3"
+        #         ),
+        #         tags= [{'key': 'environment', 'value': 'development'}],
 
-            ) 
+        #     ) 
+
+
+
+        domain = opensearchservice.Domain(self, "Domain",
+                        domain_name="fusion1",                
+                        version=opensearchservice.EngineVersion.OPENSEARCH_1_3,
+                        capacity=opensearchservice.CapacityConfig(
+                                    
+                                    master_nodes=3,
+                                    master_node_instance_type="r5.large.search",
+                                    data_nodes=3,
+                                    data_node_instance_type="r5.large.search",                            
+                                    # warm_nodes=2,
+                                    # warm_instance_type="ultrawarm1.medium.search",
+                        ),
+                        ebs=opensearchservice.EbsOptions(
+                                enabled= True,
+                                volume_size=50,
+                                # iops=3000,
+                                # volume_type=ec2.EbsDeviceVolumeType.GP2
+                        ),
+                        node_to_node_encryption=True,
+                        encryption_at_rest=opensearchservice.EncryptionAtRestOptions(
+                            enabled=True
+                        )
+                    )
+
+
+
+
+
+
 
                     
 
