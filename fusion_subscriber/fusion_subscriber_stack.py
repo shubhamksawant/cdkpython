d90afd850873cf1baa83449babce6d1e2488b79d
dev1 <jwowshubham@gmail.com>
2023-03-06 10:13:33 +0530

    added code

--- fusion_subscriber/fusion_subscriber_stack.py
+++ fusion_subscriber/fusion_subscriber_stack.py
@@ -8,17 +8,122 @@ from aws_cdk import (
     aws_opensearchservice as opensearchservice,
     aws_emrserverless as emrserverless,
     aws_scheduler as scheduler,
-     aws_iam as iam
+    aws_iam as iam,
+    aws_lambda as _lambda,
+    aws_secretsmanager as secrets
     # core
 )
 from constructs import Construct
-
+from aws_cdk import Tags
 class FusionSubscriberStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         
         
+        # alertbucket= s3.Bucket(self, "fusion-bucket-1",
+        #                   bucket_name="fusion-bucket-2",
+        #                   block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+        #                   bucket_key_enabled= False , encryption= s3.BucketEncryption.S3_MANAGED,
+        #                   versioned= True,
+        #                 #   RemovalPolicy=s3.RemovalPolicy.DESTROY                                     
+                          
+        #                   )
+        
+        # Tags.of(alertbucket).add("key1", "value1")
+        # Tags.of(alertbucket).add("key2", "value2")
+
+
+        # alertdynaomodb = dynamodb.Table(self, "MyCfnTable",
+        #                 table_name= "fusiondynamodbtable",
+        #                 encryption= dynamodb.TableEncryption.AWS_MANAGED,
+        #                 partition_key=dynamodb.Attribute(name="ref_event_uid", type=dynamodb.AttributeType.STRING),
+        #         #     replication_regions=["us-east-1", "us-east-2", "us-west-2"],
+        #                 billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+        #                 table_class=dynamodb.TableClass.STANDARD
+
+        #          )
+        
+
+        emr = emrserverless.CfnApplication(self, "MyCfnApplication",
+                release_label="emr-6.9.0",
+                name="fusion-subscriber",
+                type="Spark",
+                auto_start_configuration=emrserverless.CfnApplication.AutoStartConfigurationProperty(
+                        enabled=True
+                    ),
+                auto_stop_configuration=emrserverless.CfnApplication.AutoStopConfigurationProperty(
+                        enabled=True,
+                        idle_timeout_minutes=100
+                    ),
+                initial_capacity=[emrserverless.CfnApplication.InitialCapacityConfigKeyValuePairProperty(
+                    key="Executor",
+                    value=emrserverless.CfnApplication.InitialCapacityConfigProperty(
+                        worker_configuration=emrserverless.CfnApplication.WorkerConfigurationProperty(
+                            cpu="4 vCPU",
+                            memory="16 GB",
+                            disk="20 GB"
+                        ),
+                        worker_count=3
+                    )
+                 ),
+                 emrserverless.CfnApplication.InitialCapacityConfigKeyValuePairProperty(
+                    key="Driver",
+                    value=emrserverless.CfnApplication.InitialCapacityConfigProperty(
+                        worker_configuration=emrserverless.CfnApplication.WorkerConfigurationProperty(
+                            cpu="4 vCPU",
+                            memory="16 GB",
+                            disk="20 GB"
+                        ),
+                        worker_count=1
+                    )
+                 )],
+                 
+                 
+                maximum_capacity=emrserverless.CfnApplication.MaximumAllowedResourcesProperty(
+                        cpu="400 vCPU",
+                        memory="3000 GB",
+
+                        # the properties below are optional
+                        disk="20000 GB"
+                    ),
+
+
+                 tags= [{'key': 'environment', 'value': 'development'}],      
+                
+
+            )
+
+
+
+    #     self.build_lambda_func()
+
+    # def build_lambda_func(self):
+    #     secret_name = "secretsExample"
+    #     self.secrets_lambda = _lambda.Function(
+    #         scope=self,
+    #         id="LambdaWithSecrets",
+    #         runtime=_lambda.Runtime.PYTHON_3_9,
+    #         function_name="LambdaWithSecretsExample",
+    #         code=_lambda.Code.from_asset(
+    #             path="lambda"
+    #         ),
+    #         handler="lambda_with_secrets.handler",
+    #         # We need these env vars to access the secret inside the lambda
+    #         environment={
+    #             "secret_name": secret_name,
+    #             "secret_region": os.environ["CDK_DEFAULT_REGION"],
+    #         },
+    #     )
+    #     # Grant permission to the Lambda func to access the secret
+    #     example_secret = secrets.Secret.from_secret_name_v2(
+    #         scope=self, id="secretExample", secret_name=secret_name
+    #     )
+    #     example_secret.grant_read(grantee=self.secrets_lambda)
+
+
+
+
         # s3.CfnBucket( self, "fusion-bucket-1",bucket_name="fusion-bucket-2",
         #                 access_control="Private", 
         #                 bucket_encryption=s3.CfnBucket.BucketEncryptionProperty( 
@@ -89,86 +194,109 @@ class FusionSubscriberStack(Stack):
         #     tags= [{'key': 'environment', 'value': 'development'}],
         # )
 
+
         service_role = self.create_service_role()
 
+        my_lambda = _lambda.Function(
+            self, 
+            'FusionLambdaFunction',
+            description='Deploying Lambda Function Infrastrcture',
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            code=_lambda.Code.from_asset('lambda'),
+            role=service_role,
+            handler='hello.handler',
+            environment={  
+                        #    "bucket_name": alertbucket.bucket_name,
+                            "exerole": service_role.role_arn,
+                            # "ddbtablename": alertdynaomodb.table_name,
+                            "application": emr.attr_application_id,
+                            "app_id": emr.logical_id
 
-        scheduler.CfnSchedule(self, "MyCfnSchedule",
-                flexible_time_window=scheduler.CfnSchedule.FlexibleTimeWindowProperty(
-                    mode="OFF",
+                    }
+        )
+        
 
-                    # the properties below are optional
-                    # maximum_window_in_minutes=13
-                ),
-                schedule_expression="cron(0/3 * * * ? *)",
-                target=scheduler.CfnSchedule.TargetProperty(
-                    arn="arn:aws:lambda:us-east-2:276301730779:function:test",
-                    role_arn=service_role.role_arn,
 
-                    # the properties below are optional
-                    # dead_letter_config=scheduler.CfnSchedule.DeadLetterConfigProperty(
-                    #     arn="arn"
-                    ),
-                    # ecs_parameters=scheduler.CfnSchedule.EcsParametersProperty(
-                    #     task_definition_arn="taskDefinitionArn",
+        # service_role = self.create_service_role()
 
-                    #     # the properties below are optional
-                    #     capacity_provider_strategy=[scheduler.CfnSchedule.CapacityProviderStrategyItemProperty(
-                    #         capacity_provider="capacityProvider",
 
-                    #         # the properties below are optional
-                    #         base=123,
-                    #         weight=123
-                    #     )],
-                    #     enable_ecs_managed_tags=False,
-                    #     enable_execute_command=False,
-                    #     group="group",
-                    #     launch_type="launchType",
-                    #     network_configuration=scheduler.CfnSchedule.NetworkConfigurationProperty(
-                    #         awsvpc_configuration=scheduler.CfnSchedule.AwsVpcConfigurationProperty(
-                    #             subnets=["subnets"],
+        # scheduler.CfnSchedule(self, "MyCfnSchedule",
+        #         flexible_time_window=scheduler.CfnSchedule.FlexibleTimeWindowProperty(
+        #             mode="OFF",
 
-                    #             # the properties below are optional
-                    #             assign_public_ip="assignPublicIp",
-                    #             security_groups=["securityGroups"]
-                    #         )
-                    #     ),
-                    #     placement_constraints=[scheduler.CfnSchedule.PlacementConstraintProperty(
-                    #         expression="expression",
-                    #         type="type"
-                    #     )],
-                    #     placement_strategy=[scheduler.CfnSchedule.PlacementStrategyProperty(
-                    #         field="field",
-                    #         type="type"
-                    #     )],
-                    #     platform_version="platformVersion",
-                    #     propagate_tags="propagateTags",
-                    #     reference_id="referenceId",
-                    #     # tags=tags,
-                    #     task_count=123
-                    # ),
-                    # event_bridge_parameters=scheduler.CfnSchedule.EventBridgeParametersProperty(
-                    #     detail_type="detailType",
-                    #     source="source"
-                    # ),
-                    # input="input",
-                    # kinesis_parameters=scheduler.CfnSchedule.KinesisParametersProperty(
-                    #     partition_key="partitionKey"
-                    # ),
-                    # retry_policy=scheduler.CfnSchedule.RetryPolicyProperty(
-                    #     maximum_event_age_in_seconds=123,
-                    #     maximum_retry_attempts=123
-                    # ),
-                    # sage_maker_pipeline_parameters=scheduler.CfnSchedule.SageMakerPipelineParametersProperty(
-                    #     pipeline_parameter_list=[scheduler.CfnSchedule.SageMakerPipelineParameterProperty(
-                    #         name="name",
-                    #         value="value"
-                    #     )]
-                    # ),
-                    # sqs_parameters=scheduler.CfnSchedule.SqsParametersProperty(
-                    #     message_group_id="messageGroupId"
-                    # )
+        #             # the properties below are optional
+        #             # maximum_window_in_minutes=13
+        #         ),
+        #         schedule_expression="cron(0/3 * * * ? *)",
+        #         target=scheduler.CfnSchedule.TargetProperty(
+        #             arn=my_lambda.function_arn,
+        #             role_arn=service_role.role_arn,
 
-                )
+        #             # the properties below are optional
+        #             # dead_letter_config=scheduler.CfnSchedule.DeadLetterConfigProperty(
+        #             #     arn="arn"
+        #             ),
+        #             # ecs_parameters=scheduler.CfnSchedule.EcsParametersProperty(
+        #             #     task_definition_arn="taskDefinitionArn",
+
+        #             #     # the properties below are optional
+        #             #     capacity_provider_strategy=[scheduler.CfnSchedule.CapacityProviderStrategyItemProperty(
+        #             #         capacity_provider="capacityProvider",
+
+        #             #         # the properties below are optional
+        #             #         base=123,
+        #             #         weight=123
+        #             #     )],
+        #             #     enable_ecs_managed_tags=False,
+        #             #     enable_execute_command=False,
+        #             #     group="group",
+        #             #     launch_type="launchType",
+        #             #     network_configuration=scheduler.CfnSchedule.NetworkConfigurationProperty(
+        #             #         awsvpc_configuration=scheduler.CfnSchedule.AwsVpcConfigurationProperty(
+        #             #             subnets=["subnets"],
+
+        #             #             # the properties below are optional
+        #             #             assign_public_ip="assignPublicIp",
+        #             #             security_groups=["securityGroups"]
+        #             #         )
+        #             #     ),
+        #             #     placement_constraints=[scheduler.CfnSchedule.PlacementConstraintProperty(
+        #             #         expression="expression",
+        #             #         type="type"
+        #             #     )],
+        #             #     placement_strategy=[scheduler.CfnSchedule.PlacementStrategyProperty(
+        #             #         field="field",
+        #             #         type="type"
+        #             #     )],
+        #             #     platform_version="platformVersion",
+        #             #     propagate_tags="propagateTags",
+        #             #     reference_id="referenceId",
+        #             #     # tags=tags,
+        #             #     task_count=123
+        #             # ),
+        #             # event_bridge_parameters=scheduler.CfnSchedule.EventBridgeParametersProperty(
+        #             #     detail_type="detailType",
+        #             #     source="source"
+        #             # ),
+        #             # input="input",
+        #             # kinesis_parameters=scheduler.CfnSchedule.KinesisParametersProperty(
+        #             #     partition_key="partitionKey"
+        #             # ),
+        #             # retry_policy=scheduler.CfnSchedule.RetryPolicyProperty(
+        #             #     maximum_event_age_in_seconds=123,
+        #             #     maximum_retry_attempts=123
+        #             # ),
+        #             # sage_maker_pipeline_parameters=scheduler.CfnSchedule.SageMakerPipelineParametersProperty(
+        #             #     pipeline_parameter_list=[scheduler.CfnSchedule.SageMakerPipelineParameterProperty(
+        #             #         name="name",
+        #             #         value="value"
+        #             #     )]
+        #             # ),
+        #             # sqs_parameters=scheduler.CfnSchedule.SqsParametersProperty(
+        #             #     message_group_id="messageGroupId"
+        #             # )
+
+        #         )
         
 
 
@@ -176,7 +304,7 @@ class FusionSubscriberStack(Stack):
             return iam.Role(
                 self,
                 "eventschedulerServiceRole",
-                assumed_by=iam.ServicePrincipal("scheduler.amazonaws.com"),
+                assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
                 managed_policies=[
                     iam.ManagedPolicy(
                         self,
---
+++ lambda/lambda_with_secrets.py
@@ -0,0 +1,72 @@
+import base64
+import json
+import os
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+    """
+    Gets the secret
+    """
+    # Get env vars we passed in the stack.
+    secret_name = os.getenv("secret_name")
+    region_name = os.getenv("secret_region")
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name="secretsmanager", region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        # We need only the name of the secret
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "DecryptionFailureException":
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "InternalServiceErrorException":
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "InvalidParameterException":
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "InvalidRequestException":
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "ResourceNotFoundException":
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        else:
+            raise e
+    else:
+        # Decrypts secret using the associated KMS key.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if "SecretString" in get_secret_value_response:
+            secret = get_secret_value_response["SecretString"]
+            # If you have multiple secret values, you will need to json.loads(secret) here and then access the values using dict keys
+            return secret
+        else:
+            decoded_binary_secret = base64.b64decode(
+                get_secret_value_response["SecretBinary"]
+            )
+            return decoded_binary_secret
+
+
+def handler(event, context):
+    secret = get_secret()
+    # Don't do this in production!!!
+    return secret
